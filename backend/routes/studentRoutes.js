842fd3a79d994bd832c9d9b2f36bae05ac951cdd
Mohammed Alamin <Muhammadkabir7109@gmail.com>
2026-05-22 15:51:01 +0100

    Force git to update model file case sensitivity to lowercase

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const student = require('../models/Student');
+const student = require('../models/student');
 
 // @desc    Create a new student record
 // @route   POST /api/students
